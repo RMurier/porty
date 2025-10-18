4eae1b970160804654b247420e0e00bcecbd2072
Romain MURIER <murierromain@gmail.com>
2025-10-18 20:54:44 +0200

    feat: add missing controllers

---
+++ api/Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using api.Data.Models;
+using api.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class ProjectController : ControllerBase
+    {
+        internal IProject _project;
+        public ProjectController(IProject project)
+        {
+            _project = project ?? throw new ArgumentNullException(nameof(project));
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetALlProjects()
+        {
+            try
+            {
+                List<Project>? projects = await _project.GetAllProjects();
+                if(projects == null || projects.Count == 0)
+                {
+                    return NoContent();
+                }
+                else if (projects.Count > 0)
+                {
+                    return Ok(projects);
+                }
+                return BadRequest("Une erreur est survenue. Veuillez réessayer plus tard.");
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Une erreur est survenue. Veuillez réessayer plus tard.");
+            }
+        }
+    }
+}
---
+++ api/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using api.Data;
+using api.Data.Models;
+using api.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Services
+{
+    public class USerService : IUser
+    {
+        internal readonly PortyDbContext _ctx;
+        public USerService(PortyDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
